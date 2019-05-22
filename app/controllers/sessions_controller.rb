6bbe51e9d2bfb2c50d6fac64aba3fd734e98965c
John Cordero <johncorderox@gmail.com>
2019-05-22 15:34:40 -0700

    users can register

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,4 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   belongs_to :location
   has_secure_password
+
+  validates_format_of :username, with: /\A[a-zA-Z0-9_]+\z/, message: "can only consist of letters, numbers and underscore."
+  validates :username, uniqueness: {case_sensitive: false}
+
+  validates :email, :password, :username, presence: true
+  validates :email, uniqueness: true
 end
