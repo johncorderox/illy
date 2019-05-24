dbb7404e6e40a8edc2946355463a7274a4a50f71
John Cordero <johncorderox@gmail.com>
2019-05-24 08:20:00 -0700

    updat func

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'home#index'
-  
+
   get '/edit/:id'       => 'users#edit'
   get  '/register'      => 'users#new'
+  post '/users'         => 'users#create'
+  patch '/edit/:id'     => 'users#update'
   post '/sessions/new'  => 'sessions#new'
   delete '/logout'      => 'sessions#destroy'
+  get '/:id'            => 'users#show'
 
 end
